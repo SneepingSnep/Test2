95f38ccf7dc741283e4261563dbb8444da39c154
Sebastian <Seb@noerlev.dk>
2026-02-06 18:51:54 +0100

    jeg har lige slette noget

--- src/main.c
+++ src/main.c
@@ -1,11 +1,3 @@
-/*
- * opg1_ny_F22.c
- *
- * Created: 01-09-2020 14:34:13
- *updated 7-02-22
- * Author : osch
- */ 
-
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,9 +20,9 @@ int main(void)
   I2C_Init();  //initialize i2c interface to display
 
   InitializeDisplay(); //initialize  display
+
   clear_display();   //use this before writing you own text
-  //DDRH |= (1<<5);
-  //PORTH |= (1<<5);
+
   char HexPinstate[25];
   strcpy(HexPinstate,"PINK:0X");
   int hexlen = strlen(HexPinstate);
@@ -44,8 +36,11 @@ int main(void)
   {      
     // Alt det grønne text her er debug til at læse registre. Jeg lader det lige stå hvis det skal bruges igen
 	  //itoa(PORTK, PORTbuffer, 10);
+    
     itoa(PINK, PINbuffer, 10);
-    sendStrXY(PINbuffer,1,0);
+    sendStrXY(PINbuffer,1,0); // her printer jeg decimal tallet a register værdien
+    
+    
     //sendStrXY(PORTbuffer,0,0); 
     
     // if (firsttime == 0)
@@ -58,11 +53,12 @@ int main(void)
       {
         itoa(PINK, PINbuffer, 10);
         sendStrXY(PINbuffer,1,0);
+        
         char switchbuffer[2];
         itoa(i+1,switchbuffer,10);
         
-        sendStrXY(switchbuffer, 3,0);
-        //registerstate =true;
+        sendStrXY(switchbuffer, 3,0); // Her printer jeg hvilken switch der bliver åbnet
+
         while(!(PINK & (1<<i)))
         {
           if (!startcondition)
@@ -77,11 +73,11 @@ int main(void)
             startcondition = true;
           }
           
-          sendStrXY(HexPinstate,5,0);
+          sendStrXY(HexPinstate,5,0); // Her printer jeg hvilken hexa værdi registeret har for den givne switch
         }
         HexPinstate[hexlen] = '\0';
         startcondition = false;
-        clear_display();
+        clear_display(); // Gør klar til næste switch
       }
     }
   }
